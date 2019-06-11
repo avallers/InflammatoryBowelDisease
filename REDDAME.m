dd2cd1b8bf87d0819fa8d19962d23822195193c5
Alexander Valle Rey S <49769420+avallers@users.noreply.github.com>
2019-06-11 13:44:52 -0300

    Update REDDAME.m

--- REDDAME.m
+++ REDDAME.m
@@ -1,5 +1,11 @@
 # Machine Learning Ananlysis
 
+# InflammatoryBowelDisease
+The DATASET came from this paper: Classification of Paediatric Inflammatory Bowel Disease using Machine Learning
+https://www.nature.com/articles/s41598-017-02606-2
+https://static-content.springer.com/esm/art%3A10.1038%2Fs41598-017-02606-2/MediaObjects/41598_2017_2606_MOESM1_ESM.xls
+
+
 # Data Set
 
  endoscopic and histological data were collected from 287 patients 
